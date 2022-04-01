05f507143cfa0e873ffb549532fd030a9ac0b023
qin yu peng <qin.batista@outlook.com>
2022-03-31 17:23:39 -0700

    ss

--- Activity0/act0.cpp
+++ Activity0/act0.cpp
@@ -5,8 +5,6 @@ Optional template for obtaining user input and providing output
 Save as act0.cpp before submitting to Gradescope
 
 Add names of students in group
-
-
 yupeng qin qinyu@oregonstate.com
 
 
@@ -17,33 +15,45 @@ using namespace std;
 
 int main()
 {
+    //gcc act0.cpp -o out -lstdc++
     int A[1000];  	// array of elements <= 1000
+
     int num;		// number of elements in the array
     cin >> num;		// read in number of elements
 	cin >> A[0];	// the list will have at least one element
     for (int j = 1; j < num; j++) {
         cin >> A[j];
     }
-	// add your code
 
+	// add your code
+    // int A[5]={1,9,10,5,6};
+    // int num = 5;
     //find smallest element
-    int smallest_int = 0;
+    int smallest_int = 9999;
     int smallest_int_index = 0;
     int biggest_int = 0;
     int biggest_int_index = 0;
-    if(int i = 0; i<num; i++)
+
+    //find smallest integer and largest integer at same time
+    for(int i = 0; i<num; i++)
     {
         if(A[i]<smallest_int)
         {
             smallest_int = A[i];
             smallest_int_index = i;
+            // printf("smallest_int: %d\n",smallest_int);
         }
         if(A[i]>biggest_int)
         {
             biggest_int = A[i];
             biggest_int_index = i;
+            // printf("biggest_int: %d\n",biggest_int);
         }
     }
+    printf("smallest:%d\n",smallest_int);
+    printf("biggest:%d\n",biggest_int);
+
+    //if the left and the right of the smallest integer is big value, return true, else return false
     if(A[biggest_int_index-1] == smallest_int || (A[biggest_int_index+1] == smallest_int))
     {
         // the largest element is next to the smallest
