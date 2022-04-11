ef31783dc3c7128a3bae80d085b5645805d35ef4
qin yu peng <qin.batista@outlook.com>
2022-04-11 14:51:28 -0700

    ss

--- Activity1/act1.cpp
+++ Activity1/act1.cpp
@@ -19,30 +19,31 @@ using namespace std;
 
 int main()
 {
-    // int A[1000];  	// array of elements <= 1000
-    // int num;		// number of elements in the array
-    // cin >> num;		// read in number of elements
-	// cin >> A[0];	// the list will have at least one element
-    //  for (int j = 1; j < num; j++) {
-    //     cin >> A[j];
-    // }
-    int A[100];
-    int num = 15;
-    A[0]= 7;
-    A[1]= 8;
-    A[2]= 1;
-    A[3]= 3;
-    A[4]= 4;
-    A[5]= 8;
-    A[6]= 3;
-    A[7]= 1;
-    A[8]= 8;
-    A[9]= 1;
-    A[10]= 10;
-    A[11]= 23;
-    A[12]= 3;
-    A[13]= 8;
-    A[14]= 3;
+    int A[1000];  	// array of elements <= 1000
+    int num;		// number of elements in the array
+    cin >> num;		// read in number of elements
+	cin >> A[0];	// the list will have at least one element
+     for (int j = 1; j < num; j++) {
+        cin >> A[j];
+    }
+    // 7 8 1 3 4 8 3 1 8 1 10 23 3 8 3
+    // int A[100];
+    // int num = 15;
+    // A[0]= 7;
+    // A[1]= 8;
+    // A[2]= 1;
+    // A[3]= 3;
+    // A[4]= 4;
+    // A[5]= 8;
+    // A[6]= 3;
+    // A[7]= 1;
+    // A[8]= 8;
+    // A[9]= 1;
+    // A[10]= 10;
+    // A[11]= 23;
+    // A[12]= 3;
+    // A[13]= 8;
+    // A[14]= 3;
     int count = 0;
     int freq[num];
     for(int i=0; i<num; i++)
